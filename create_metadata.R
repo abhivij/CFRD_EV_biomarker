0a28a432e4af00a05ae3ea580f6214c150d578c9
abhivij <abhishek.kcvijayan@gmail.com>
2022-03-25 14:45:05 +1100

    added year

--- create_metadata.R
+++ create_metadata.R
@@ -9,7 +9,7 @@ sample_info <- read.table("data/WAT10166-WAT10180-WAT9739_md5sum.txt", sep = " "
   separate(V3, into = c(NA, NA, NA, "file_name"), sep = "/") %>%
   separate("file_name", into = c("sample_long_name", NA), sep = "_L") %>%
   unique() %>%
-  separate("sample_long_name", into = c("sample_name", "illumina_sample_number"), sep = "_S") %>%
+  separate("sample_long_name", into = c("sample_name", "illumina_sample_number"), sep = "_S", remove = FALSE) %>%
   mutate(illumina_sample_number = strtoi(illumina_sample_number)) %>%
   arrange(sample_name)
 sample_info <- sample_info %>%
@@ -28,7 +28,30 @@ sample_info <- sample_info %>%
     grepl("11_", sample_name, fixed = TRUE) ~ substr(sample_name, 7, 11),
     grepl("14E_", sample_name, fixed = TRUE) ~ substr(sample_name, 8, 12),
     TRUE ~ NA_character_))
+sample_info <- sample_info %>%
+  mutate(year = case_when(
+    grepl("2020_t0_CPH", sample_name, fixed = TRUE) ~ "2020",
+    grepl("2017_CPH", sample_name, fixed = TRUE) ~ "2017",
+    grepl("CPH", sample_name, fixed = TRUE) ~ "unknown",
+    grepl("17_", sample_name, fixed = TRUE) ~ paste0("20", substr(sample_name, 4, 5)),  # 17_20_#####
+    grepl("11_", sample_name, fixed = TRUE) ~ paste0("20", substr(sample_name, 4, 5)),
+    grepl("14E_", sample_name, fixed = TRUE) ~ paste0("20", substr(sample_name, 5, 6)),
+    TRUE ~ NA_character_))
 
 write.csv(sample_info, "data/formatted/sample_info_temp.txt", row.names = FALSE)
 
-proteomic_metadata <- read.table("data/proteomics_sinfo.txt", header = TRUE, sep = "\t")
+proteomic_metadata <- read.table("data/proteomics_sinfo.txt", header = TRUE, sep = "\t") %>%
+  mutate(individualid = gsub("CPH0", "CPH", individualid, fixed = TRUE)) %>%
+  filter(!individualid %in% c("glufib", "QC")) %>%
+  select(c(individualid, condition, cohort, agegroup, modulator, biotype, year, FEV1)) %>%
+  unique() 
+
+write.csv(proteomic_metadata, "data/proteomic_processing.txt", row.names = FALSE)
+
+proteomic_metadata <- proteomic_metadata %>%
+  group_by(individualid, year) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n))
+write.csv(proteomic_metadata, "data/proteomic_multiple_entries.txt", row.names = FALSE)
+
+length(unique(proteomic_metadata$individualid))
