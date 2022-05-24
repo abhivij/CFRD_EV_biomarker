b543cc9cf729f9e242b2da26c7e33da6a414b6ca
abhivij <abhishek.kcvijayan@gmail.com>
2022-05-24 15:12:54 +1000

    updated cf vs hc

--- de_analysis.R
+++ de_analysis.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 library(edgeR)
 library(EnhancedVolcano)
+library("ggvenn")
 
 base_dir <- "/home/abhivij/UNSW/VafaeeLab/CysticFibrosisGroup/ExoCF/CFRD_EV_biomarker/"
 setwd(base_dir)
+
+source("utils.R")
 
 
 #read transcriptomics data (miRNA)
@@ -21,81 +24,85 @@ sum(is.na(umi_counts))
 
 #CF Vs HC all samples
 meta_data_subset <- meta_data %>%
-  mutate(de_condition = case_when(!is.na(pre_post_modulator) ~ "modulator_effect",
-                                  condition == "CF_pre_post_modulator" ~ "modulator_effect",
-                                  condition == "HC" ~ "HC",
+  filter(is.na(pre_post_modulator) | pre_post_modulator != 1) %>%
+  mutate(de_condition = case_when(condition == "HC" ~ "HC",
                                   TRUE ~ "CF")) %>%
-  arrange(de_condition)
+  arrange(condition)
 
 counts <- umi_counts[, meta_data_subset$sample_long_name]
-group <- factor(meta_data_subset$de_condition, levels = c("HC", "CF", "modulator_effect"))
+group <- factor(meta_data_subset$de_condition, levels = c("HC", "CF"))
 
-y <- DGEList(counts = counts, 
-             group = group)
-keep <- filterByExpr(y)
-y <- y[keep, , keep.lib.sizes=FALSE]
-y
-# head(y$counts)
 
-y <- calcNormFactors(y)
-y
+de_analysis(
+  counts = counts,
+  group = group,
+  results_dir_path = "de_results",
+  plot_title = "CF Vs HC",
+  p_val_cutoff = 0.05,
+  logFC_cutoff = 0.5,
+  k = 5
+)
 
-design <- model.matrix(~group)
-y <- estimateDisp(y, design)
-
-fit <- glmQLFit(y, design)
-
-qlf.2vs1 <- glmQLFTest(fit, coef=2)
-de_rnas <- topTags(qlf.2vs1, n = Inf)$table
-# EnhancedVolcano(de_rnas,
-#                 lab = row.names(de_rnas),
-#                 x = 'logFC',
-#                 y = 'FDR', pCutoff = 0.05,
-#                 title = "CF Vs HC",
-#                 labSize = 3,
-#                 col = c("grey", "grey", "blue", "red"),
-#                 colAlpha = 1)
-
-de_rnas <- de_rnas %>%
- rownames_to_column("Molecule")
-
-# result = de_rnas
-# title = "CF Vs HC"
-# file_name = "volcano_CFVsHC.png"
-# dir_path = "plots"
-# p_val_cutoff = 0.05 
+# counts
+# group
+# results_dir_path = "de_results"
+# plot_title = "CF Vs HC"
+# p_val_cutoff = 0.05
 # logFC_cutoff = 0.5
-# p_val_column = "FDR"
 # k = 5
-
-create_volcano_plot(
-  result = de_rnas,
-  title = "CF Vs HC",
-  file_name = "volcano_CFVsHC_fdr.png",
-  dir_path = "de_results",
-  p_val_cutoff = 0.05, 
-  logFC_cutoff = 0.5,
-  p_val_column = "FDR",
-  k = 5
-)
-
-create_volcano_plot(
-  result = de_rnas,
-  title = "CF Vs HC",
-  file_name = "volcano_CFVsHC_pval.png",
-  dir_path = "de_results",
-  p_val_cutoff = 0.05, 
-  logFC_cutoff = 0.5,
-  p_val_column = "PValue",
-  k = 5
-)
-
-dir_path <- "de_results"
-file_name <- "DE_CFVsHC.csv"
-significant_de <- de_rnas %>%
-  filter(FDR < 0.05) %>%
-  arrange(logFC)
-write.csv(significant_de, paste(dir_path, file_name, sep = "/"), row.names = FALSE)
+de_analysis <- function(counts, group, results_dir_path,
+                        plot_title, p_val_cutoff, logFC_cutoff, k = 5, coef = 2){
+  y <- DGEList(counts = counts, 
+               group = group)
+  keep <- filterByExpr(y)
+  y <- y[keep, , keep.lib.sizes=FALSE]
+  y
+  # head(y$counts)
+  
+  y <- calcNormFactors(y)
+  y
+  
+  design <- model.matrix(~group)
+  y <- estimateDisp(y, design)
+  
+  fit <- glmQLFit(y, design)
+  
+  qlf <- glmQLFTest(fit, coef=coef)
+  de_rnas <- topTags(qlf, n = Inf)$table
+  
+  de_rnas <- de_rnas %>%
+    rownames_to_column("Molecule")
+  
+  p_val_column = "FDR"
+  create_volcano_plot(
+    result = de_rnas,
+    title = plot_title,
+    file_name = paste0("volcano_", gsub(" ", "", plot_title, fixed = TRUE),
+                       "_", p_val_column, ".png"),
+    dir_path = results_dir_path,
+    p_val_cutoff = p_val_cutoff, 
+    logFC_cutoff = logFC_cutoff,
+    p_val_column = p_val_column,
+    k = k
+  )
+  p_val_column = "PValue"
+  create_volcano_plot(
+    result = de_rnas,
+    title = plot_title,
+    file_name = paste0("volcano_", gsub(" ", "", plot_title, fixed = TRUE),
+                       "_", p_val_column, ".png"),
+    dir_path = results_dir_path,
+    p_val_cutoff = p_val_cutoff, 
+    logFC_cutoff = logFC_cutoff,
+    p_val_column = p_val_column,
+    k = k
+  )
+  file_name <- paste0("DE_", gsub(" ", "", plot_title, fixed = TRUE), ".csv")
+  significant_de <- de_rnas %>%
+    filter(PValue < 0.05) %>%
+    arrange(logFC)
+  write.csv(significant_de, paste(results_dir_path, file_name, sep = "/"), row.names = FALSE)  
+}
 
 
 ###############
@@ -103,3 +110,144 @@ write.csv(significant_de, paste(dir_path, file_name, sep = "/"), row.names = FAL
 #sample design matrix
 group <- factor(c(1,1,2,2,3,3))
 design <- model.matrix(~group)
+
+
+
+##############
+
+
+#CF Vs HC child samples
+
+meta_data_subset <- meta_data %>%
+  filter(is.na(pre_post_modulator) | pre_post_modulator != 1) %>%
+  filter(age_group == "child") %>%
+  mutate(de_condition = case_when(condition == "HC" ~ "HC",
+                                  TRUE ~ "CF")) %>%
+  arrange(de_condition)
+
+
+counts <- umi_counts[, meta_data_subset$sample_long_name]
+group <- factor(meta_data_subset$de_condition, levels = c("HC", "CF"))
+
+
+de_analysis(
+  counts = counts,
+  group = group,
+  results_dir_path = "de_results",
+  plot_title = "CF Vs HC child samples",
+  p_val_cutoff = 0.05,
+  logFC_cutoff = 0.5,
+  k = 5
+)
+
+
+
+#CF Vs HC adult samples
+meta_data_subset <- meta_data %>%
+  filter(is.na(pre_post_modulator) | pre_post_modulator != 1) %>%
+  filter(age_group == "adult") %>%
+  mutate(de_condition = case_when(condition == "HC" ~ "HC",
+                                  TRUE ~ "CF")) %>%
+  arrange(de_condition)
+
+counts <- umi_counts[, meta_data_subset$sample_long_name]
+group <- factor(meta_data_subset$de_condition, levels = c("HC", "CF"))
+
+
+de_analysis(
+  counts = counts,
+  group = group,
+  results_dir_path = "de_results",
+  plot_title = "CF Vs HC adult samples",
+  p_val_cutoff = 0.05,
+  logFC_cutoff = 0.5,
+  k = 5
+)
+
+
+de <- read.csv("de_results/DE_CFVsHC.csv")
+de_adult <- read.csv("de_results/DE_CFVsHCadultsamples.csv")
+de_child <- read.csv("de_results/DE_CFVsHCchildsamples.csv")
+
+de_up <- de %>%
+  filter(logFC > 0)
+de_down <- de %>%
+  filter(logFC < 0)
+
+de_adult_up <- de_adult %>%
+  filter(logFC > 0)
+de_adult_down <- de_adult %>%
+  filter(logFC < 0)
+
+de_child_up <- de_child %>%
+  filter(logFC > 0)
+de_child_down <- de_child %>%
+  filter(logFC < 0)
+
+
+ggvenn(list("CF Vs HC Up Reg" = de_up$Molecule, 
+            "CF Vs HC Adult Up Reg" = de_adult_up$Molecule,
+            "CF Vs HC Child Up Reg" = de_child_up$Molecule),
+       stroke_size = 0.1,
+       set_name_size = 5,
+       text_size = 3, )
+ggsave("de_results/CFVsHC_up_reg_venn.png")
+
+Reduce(intersect, list(de_up$Molecule, de_adult_up$Molecule, de_child_up$Molecule))
+
+
+ggvenn(list("CF Vs HC Down Reg" = de_down$Molecule, 
+            "CF Vs HC Adult Down Reg" = de_adult_down$Molecule,
+            "CF Vs HC Child Down Reg" = de_child_down$Molecule),
+       stroke_size = 0.1,
+       set_name_size = 5,
+       text_size = 3, )
+ggsave("de_results/CFVsHC_down_reg_venn.png")
+
+Reduce(intersect, list(de_down$Molecule, de_adult_down$Molecule, de_child_down$Molecule))
+
+
+
+###############
+
+#CFRD Vs IGT
+meta_data_subset <- meta_data %>%
+  mutate(de_condition = case_when(!is.na(pre_post_modulator) ~ "CF",
+                                  condition == "CF_pre_post_modulator" ~ "CF",
+                                  condition == "CF_non_modulator" ~ "CF",
+                                  condition == "IND" ~ "CF",
+                                  TRUE ~ condition)) %>%
+  arrange(de_condition)
+summary(factor(meta_data_subset$de_condition))
+
+
+counts <- umi_counts[, meta_data_subset$sample_long_name]
+group <- factor(meta_data_subset$de_condition, levels = c("CFRD", "IGT", "NGT", "CF", "HC"))
+
+
+counts = counts
+group = group
+results_dir_path = "de_results"
+plot_title = "CFRD Vs IGT adult samples"
+p_val_cutoff = 0.05
+logFC_cutoff = 0.5
+k = 5
+coef = 1:2
+
+de_analysis(
+  counts = counts,
+  group = group,
+  results_dir_path = "de_results",
+  plot_title = "CFRD Vs IGT adult samples",
+  p_val_cutoff = 0.05,
+  logFC_cutoff = 0.5,
+  k = 5,
+  coef = 1:2
+)
+
+
+
+#CFRD Vs NGT
+
+
+#IGT Vs NGT
