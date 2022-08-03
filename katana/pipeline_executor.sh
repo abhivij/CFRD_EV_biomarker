38e351798df728e46d0e0aced9554157e1135111
abhivij <abhishek.kcvijayan@gmail.com>
2022-08-03 11:29:30 +1000

    au subset execution

--- katana/pipeline_executor.sh
+++ katana/pipeline_executor.sh
@@ -15,3 +15,8 @@ qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -
 qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 37-40 pipeline_executor.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 41-44 pipeline_executor.pbs
 qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 45-48 pipeline_executor.pbs
+
+
+
+
+qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 49-58 pipeline_executor.pbs
