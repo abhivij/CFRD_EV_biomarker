fb53f3b84649fbada769dea1ca772e66960b976b
abhivij <abhishek.kcvijayan@gmail.com>
2022-08-16 21:10:13 +1000

    CFRDVsIGT

---
+++ prediction_pipeline/cm_logistic_regression.R
@@ -0,0 +1,94 @@
+# label.train <- output_labels.train
+# label.test <- output_labels.test
+# regularize <- "l2"
+
+log_reg_model <- function(data.train, label.train, data.test, label.test, 
+                          classes, regularize = NA, ...){
+  
+  model_name <- "logistic regression"
+  if (is.na(regularize)) {
+    model_name <- paste("Simple", model_name)
+  } else if(regularize == 'l1') {
+    model_name <- paste("L1 Regularized", model_name)
+  } else {
+    model_name <- paste("L2 Regularized", model_name)
+  }
+  
+  #setting default value for metrics, to handle case where unable to train / execute classification model
+  metrics <- c(0, 0) 
+  
+  try({
+    label.train$Label <- ifelse(label.train$Label == classes[1], 0, 1)
+    label.test$Label <- ifelse(label.test$Label == classes[1], 0, 1)
+    
+    if (!is.na(regularize)) {
+      #alpha = 1 => l1 regularization (lasso)
+      #alpha = 0 => l2 regularization (ridge)
+      if(regularize == 'l1') {
+        alpha <- 1
+      }
+      else {
+        alpha <- 0
+      }
+      set.seed(1000)
+      model <- glmnet::cv.glmnet(as.matrix(data.train), label.train$Label, alpha = alpha, family = 'binomial', type.measure = 'mse')
+      # plot(model)
+      
+      lambda_min <- model$lambda.min
+      lambda_1se <- model$lambda.1se
+      
+      best_acc <- -1
+      best_cut_off <- 0.3
+      for(cut_off in seq(0.2, 0.8, 0.01)){
+        print(cut_off)
+        pred_prob.train <- predict(model, newx = as.matrix(data.train), s = lambda_1se, type = 'response')
+        pred.train <- ifelse(pred_prob.train > cut_off, 1, 0)
+        acc <- mean(pred.train == label.train$Label)
+        if(acc > best_acc){
+          best_acc <- acc
+          best_cut_off <- cut_off
+        }
+      }
+      
+      pred_prob.train <- predict(model, newx = as.matrix(data.train), s = lambda_1se, type = 'response')
+      pred.train <- ifelse(pred_prob.train > best_cut_off, 1, 0)
+      mean(pred.train == label.train$Label)
+      
+      pred_prob <- predict(model, newx = as.matrix(data.test), s = lambda_1se, type = 'response')
+      pred <- ifelse(pred_prob > best_cut_off, 1, 0)
+      mean(pred == label.test$Label)
+    }
+    else {
+      model <- glm(label.train$Label ~., data = data.train, family = binomial)
+      pred_prob <- predict(model, newdata = data.test, type = 'response')
+      pred <- ifelse(pred_prob > 0.5, 1, 0)
+    }
+
+    result_df.train <- data.frame("TrueLabel" = ifelse(label.train$Label == 0, classes[1], classes[2]),
+                            "Pred_prob" = pred_prob.train[,1],
+                            "PredictedLabel" = pred.train[,1],
+                            "Type" = "train",
+                            "cutoff" = best_cut_off)
+
+    result_df.test <- data.frame("TrueLabel" = ifelse(label.test$Label == 0, classes[1], classes[2]),
+                             "Pred_prob" = pred_prob[,1],
+                             "PredictedLabel" = pred[,1],
+                             "Type" = "test",
+                             "cutoff" = best_cut_off)
+    
+    result_df <- rbind(result_df.train %>%
+                         rownames_to_column("sample"), 
+                       result_df.test %>%
+                         rownames_to_column("sample"))
+    
+    result_df <- result_df %>%
+      mutate(Pred_prob = as.double(Pred_prob)) %>%
+      mutate(PredictedLabel = ifelse(PredictedLabel == 0, classes[1], classes[2]))
+    
+    return (result_df)
+  })
+  
+  
+}
+
+
---
+++ prediction_pipeline/pipeline.R
@@ -0,0 +1,178 @@
+library(tidyverse)
+
+base_dir <- "~/UNSW/VafaeeLab/CysticFibrosisGroup/ExoCF/CFRD_EV_biomarker/"
+setwd(base_dir)
+
+
+source("prediction_pipeline/cm_logistic_regression.R")
+source("prediction_pipeline/cm_svm.R")
+source("prediction_pipeline/cm_rf.R")
+
+######################################################################
+
+
+# comparison = "CFRDVsIGT"
+# classes = c("IGT", "CFRD")
+# best_features_file_path = "data/selected_features/best_features_with_is_best.csv"
+# dataset_replace_str = "CF_EV_AU_zlogtmm_"
+# 
+# result_file_dir = "data/prediction_result/"
+# result_file_name = "CFRDVsIGT.csv"
+
+
+pipeline <- function(comparison, classes, 
+                     best_features_file_path,
+                     dataset_replace_str = "CF_EV_AU_zlogtmm_",
+                     result_file_dir = "data/prediction_result/",
+                     result_file_name = "CFRDVsIGT.csv",
+                     perform_filter = TRUE,
+                     norm = "norm_log_tmm"){
+  
+  best_features <- read.csv(best_features_file_path)  
+  best_features_sub <- best_features %>%
+    mutate(dataset_id = gsub(dataset_replace_str, "", dataset_id)) %>%
+    filter(is_best == 1, dataset_id == comparison)
+  
+  biomarkers <- strsplit(best_features_sub$biomarkers, split = "|", fixed = TRUE)[[1]]  
+  
+  data <- read.table("data/formatted/umi_counts.csv", header=TRUE, sep=",", row.names=1, skip=0,
+                     nrows=-1, comment.char="", fill=TRUE, na.strings = "NA")
+  phenotype <- read.table("data/formatted/phenotype.txt", header=TRUE, sep="\t")
+  
+  output_labels.train <- phenotype %>%
+    rename("Label" = comparison) %>%
+    filter(Label %in% classes, country == "AU") %>%
+    dplyr::select(Sample, Label) %>%
+    arrange(Label, Sample)
+  data.train <- data[, output_labels.train$Sample]
+
+  output_labels.test <- phenotype %>%
+    rename("Label" = comparison) %>%
+    filter(Label %in% classes, country == "DK") %>%
+    dplyr::select(Sample, Label) %>%
+    arrange(Label, Sample)
+  data.test <- data[, output_labels.test$Sample]
+  
+  #currently data.train, data.test format : (transcripts x samples)
+  
+  if(perform_filter){
+    keep <- edgeR::filterByExpr(data.train, group = output_labels.train$Label)
+    data.train <- data.train[keep, ]
+    data.test <- data.test[keep, ]  
+  }
+  
+  
+  if(norm == "norm_log_tmm"){
+    #calculating norm log tmm
+    dge <- edgeR::DGEList(counts = data.train, group = output_labels.train$Label)
+    dge <- edgeR::calcNormFactors(dge, method = "TMM")
+    tmm <- edgeR::cpm(dge, log = TRUE)
+    data.train <- tmm
+    
+    dge <- edgeR::DGEList(counts = data.test, group = output_labels.test$Label)
+    dge <- edgeR::calcNormFactors(dge, method = "TMM")
+    tmm <- edgeR::cpm(dge, log = TRUE)
+    data.test <- tmm
+    
+    data.train <- as.data.frame(t(as.matrix(data.train)))
+    data.test <- as.data.frame(t(as.matrix(data.test)))  
+    
+    #normalizing the data
+    normparam <- caret::preProcess(data.train) 
+    data.train <- predict(normparam, data.train)
+    data.test <- predict(normparam, data.test) #normalizing test data using params from train data    
+  }
+  
+  #now data.train, data.test format : (samples x transcripts)
+  
+  #get best biomarkers only
+
+  
+  features_with_slash <- colnames(data.train)[grepl("/", colnames(data.train), fixed = TRUE)] 
+  for(f in features_with_slash){
+    f_replaced <- gsub("/|-", ".", f) 
+    if(f_replaced %in% biomarkers){
+      biomarkers[biomarkers == f_replaced] = f
+    }
+  }
+  biomarkers <- gsub(".", "-", biomarkers, fixed = TRUE)
+  
+  
+  data.train <- data.train[, biomarkers]
+  data.test <- data.test[, biomarkers]
+
+  if(comparison == "CFRDVsIGT"){
+    #l2 log reg
+    result_df <- log_reg_model(data.train, output_labels.train, data.test, output_labels.test, classes, regularize = "l2") 
+  } else if(comparison == "CFRDVsNGT"){
+    #sigmoid kernel svm
+    result_df <- svm_model(data.train, output_labels.train, data.test, output_labels.test, classes, kernel = "sigmoid")
+  } else if(comparison == "IGTVsNGT"){
+    #random forest
+    result_df <- rf_model(data.train, output_labels.train, data.test, output_labels.test, classes)
+  }
+
+  if(!dir.exists(result_file_dir)){
+    dir.create(result_file_dir, recursive = TRUE)
+  }
+  write.csv(format(result_df, digits = 3), paste0(result_file_dir, result_file_name), row.names = FALSE)
+  
+}  
+
+
+
+pipeline(
+  comparison = "CFRDVsIGT",
+  classes = c("IGT", "CFRD"),
+  best_features_file_path = "data/selected_features/best_features_with_is_best.csv",
+  dataset_replace_str = "CF_EV_AU_zlogtmm_",
+  result_file_dir = "data/prediction_result/",
+  result_file_name = "CFRDVsIGT.csv",
+  perform_filter = TRUE,
+  norm = "norm_log_tmm"
+)
+
+
+
+show_metrics <- function(comparison, classes, result_file_path){
+  print(comparison)
+  result_df <- read.csv(result_file_path)  
+  
+  results.train <- result_df %>%
+    filter(Type == "train")
+  results.test <- result_df %>%
+    filter(Type == "test")
+  
+  acc.train <- mean(results.train$TrueLabel == results.train$PredictedLabel)
+  acc.test <- mean(results.test$TrueLabel == results.test$PredictedLabel)
+  
+  pr <- ROCR::prediction(results.train$Pred_prob, results.train$TrueLabel, label.ordering = classes)
+  auc.train <- ROCR::performance(pr, measure = "auc")@y.values[[1]]
+  
+  pr <- ROCR::prediction(results.test$Pred_prob, results.test$TrueLabel, label.ordering = classes)
+  auc.test <- ROCR::performance(pr, measure = "auc")@y.values[[1]]
+
+  metrics <- data.frame(Comparison = comparison,
+                        TrainAccuracy = acc.train,
+                        TrainAUC = auc.train,
+                        TestAccuracy = acc.test,
+                        TestAUC = auc.test)
+  
+  file_path = "data/prediction_result/metrics.csv"
+  write.table(x = metrics, file = file_path, append = TRUE, 
+              col.names = !file.exists(file_path), sep = ",",
+              row.names = FALSE)
+    
+}
+
+
+
+#CFRDVsIGT
+
+# comparison = "CFRDVsIGT"
+# result_file_path <- "data/prediction_result/CFRDVsIGT.csv"
+
+
+show_metrics(comparison = "CFRDVsIGT",
+             classes = c("IGT", "CFRD"),
+             result_file_path = "data/prediction_result/CFRDVsIGT.csv")
