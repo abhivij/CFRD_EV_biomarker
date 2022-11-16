6e050905979ac22e6b17c54fa65df0ad0dddfd2b
abhivij <abhishek.kcvijayan@gmail.com>
2022-11-16 17:29:19 +1100

    more dataset_args

--- katana/pipeline_executor.sh
+++ katana/pipeline_executor.sh
@@ -36,4 +36,4 @@ qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -
 qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 91-94 pipeline_executor.pbs
 
 
-qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 95-100 pipeline_executor.pbs
+qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 95-102 pipeline_executor.pbs
