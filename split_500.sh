7bc4c376ed4f04b4d9484abeb8d70cbacded86fa
abhivij <abhishek.kcvijayan@gmail.com>
2023-10-13 13:15:05 +1100

    raw file split script for upload to qiagen

---
+++ split_500.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /srv/scratch/vafaeelab/AbhishekVijayan/exoCFRD_miRNA/WAT10166-WAT10180-WAT9739-single-level/
+i=0
+for f_name in *
+do
+ dir_name="dir_new"$(( (i/500) + 1 ))
+ echo $dir_name
+ echo $f_name
+ dir_path="../"$dir_name
+ if [ ! -d "$dir_path" ]; then
+  mkdir $dir_path
+ fi
+ cp $f_name $dir_path"/"
+ i=$((i+1))
+done
