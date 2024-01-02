79e52aa82c3fe416157a556d8071898c50c4b845
abhivij <abhishek.kcvijayan@gmail.com>
2024-01-02 12:21:50 +1100

    change dir path for newly processed data

--- make_single_level.sh
+++ make_single_level.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /srv/scratch/vafaeelab/AbhishekVijayan/exoCFRD_miRNA_2023/UMA12487
+cd /srv/scratch/vafaeelab/AbhishekVijayan/exoCFRD_miRNA_2023/updated
 mkdir UMA12487_single_level
 
 for f_name in UMA12487/*/*
