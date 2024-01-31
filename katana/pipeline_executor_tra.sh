54a2459aa0184e5921c71989fc9fd9268c3a6fa5
abhivij <abhishek.kcvijayan@gmail.com>
2024-01-31 12:35:16 +1100

    tra pipeline execution

--- katana/pipeline_executor_tra.sh
+++ katana/pipeline_executor_tra.sh
@@ -7,3 +7,11 @@ qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -
 qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 13-16 pipeline_executor_tra.pbs
 qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 17-26 pipeline_executor_tra.pbs
 qsub -l select=1:ncpus=4:mem=124gb,walltime=4:00:00 -M a.vijayan@unsw.edu.au -m ae -J 27-30 pipeline_executor_tra.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 31-34 pipeline_executor_tra.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 35-38 pipeline_executor_tra.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 39-42 pipeline_executor_tra.pbs
+
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 43-46 pipeline_executor_tra.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 47-50 pipeline_executor_tra.pbs
+qsub -l select=1:ncpus=16:mem=124gb,walltime=12:00:00 -M a.vijayan@unsw.edu.au -m ae -J 51-54 pipeline_executor_tra.pbs
